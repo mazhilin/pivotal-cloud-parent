99dd1807d2b3f37e499813cd3d35c0b393494f16
marklin <2273990296@qq.com>
2023-07-11 00:53:53 +0800

    [master 2022-07-11 星期二 00:28]:提交parent工程模块

---
+++ pivotal-cloud-common/pivotal-cloud-common-domain/src/main/java/com/pivotal/cloud/common/dao/service/BaseService.java
@@ -0,0 +1,14 @@
+package com.pivotal.cloud.common.dao.service;
+
+/**
+ * @className: com.pivotal.cloud.common.dao.service.BaseService
+ * @projectName: 封装PivotalCloud项目-BaseService类
+ * @module: PivotalCloud项目-BaseService类，主要位于BaseService模块的业务场景
+ * @content: BaseService类，主要用于完成BaseService的封装和定义.
+ * @author: Powered by Marklin
+ * @datetime: 2023-07-11 00:50
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 PivotalCloud Systems Incorporated. All rights reserved.
+ */
+public interface BaseService {
+}
---
+++ pivotal-cloud-common/pivotal-cloud-common-domain/src/main/java/com/pivotal/cloud/common/dao/service/impl/BaseServiceImpl.java
@@ -0,0 +1,16 @@
+package com.pivotal.cloud.common.dao.service.impl;
+
+import com.pivotal.cloud.common.dao.service.BaseService;
+
+/**
+ * @className: com.pivotal.cloud.common.dao.service.impl.ServiceImpl
+ * @projectName: 封装PivotalCloud项目-ServiceImpl类
+ * @module: PivotalCloud项目-ServiceImpl类，主要位于ServiceImpl模块的业务场景
+ * @content: ServiceImpl类，主要用于完成ServiceImpl的封装和定义.
+ * @author: Powered by Marklin
+ * @datetime: 2023-07-11 00:52
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 PivotalCloud Systems Incorporated. All rights reserved.
+ */
+public class BaseServiceImpl implements BaseService {
+}
