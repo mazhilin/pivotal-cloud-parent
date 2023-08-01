b5dc56863376ca5343a8e802c60b446e41ef9c35
marklin <2273990296@qq.com>
2023-08-02 03:18:33 +0800

    [master 2022-08-02 星期三 01:37]:提交parent工程模块

--- pivotal-cloud-boot/src/main/java/com/pivotal/cloud/boot/holder/SpringContextHolder.java
+++ pivotal-cloud-boot/src/main/java/com/pivotal/cloud/boot/holder/SpringContextHolder.java
@@ -3,12 +3,15 @@ package com.pivotal.cloud.boot.holder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 /**
  * @className: com.pivotal.cloud.boot.holder.SpringContextHolder
@@ -29,7 +32,49 @@ public class SpringContextHolder implements BeanFactoryPostProcessor, Applicatio
 
     private static ApplicationContext applicationContext = null;
 
+    /**
+     * 取得存储在静态变量中的ApplicationContext.
+     */
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
 
+    public static ListableBeanFactory getBeanFactory() {
+        return null == beanFactory ? applicationContext : beanFactory;
+    }
+
+    /**
+     * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String name) {
+        return (T) getBeanFactory().getBean(name);
+    }
+
+    /**
+     * 从静态变量applicationContext中取得Bean, Map<Bean名称，实现类></>
+     */
+    public static <T> Map<String, T> getBeansOfType(Class<T> type) {
+        return getBeanFactory().getBeansOfType(type);
+    }
+
+    /**
+     * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
+     */
+    public static <T> T getBean(Class<T> requiredType) {
+        return getBeanFactory().getBean(requiredType);
+    }
+
+
+    /**
+     * 清除SpringContextHolder中的ApplicationContext为Null.
+     */
+    public static void clearHolder() {
+        if (log.isDebugEnabled()) {
+            log.debug("清除SpringContextHolder中的ApplicationContext:" + applicationContext);
+        }
+        applicationContext = null;
+    }
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
