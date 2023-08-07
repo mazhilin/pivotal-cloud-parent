46d5b7515b098a0368e6cc60db34e2b9d89642fc
marklin <2273990296@qq.com>
2023-08-08 02:33:15 +0800

    [master 2022-08-08 星期二 01:37]:提交parent工程模块

---
+++ pivotal-cloud-component/pivotal-cloud-gateway/pivotal-cloud-gateway-boot/src/main/java/com/pivotal/cloud/gateway/boot/properties/GatewayBootProperties.java
@@ -0,0 +1,39 @@
+package com.pivotal.cloud.gateway.boot.properties;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @className: com.pivotal.cloud.gateway.boot.properties.PivotalCloudGatewayProperties
+ * @projectName: 封装PivotalCloud项目-PivotalCloudGatewayProperties类
+ * @module: PivotalCloud项目-PivotalCloudGatewayProperties类，主要位于PivotalCloudGatewayProperties模块的业务场景
+ * @content: PivotalCloudGatewayProperties类，主要用于完成PivotalCloudGatewayProperties的封装和定义.
+ * @author: Powered by Marklin
+ * @datetime: 2023-08-08 02:24
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 PivotalCloud Systems Incorporated. All rights reserved.
+ */
+@Data
+@Component
+@Slf4j
+@ConfigurationProperties(prefix = GatewayBootProperties.PREFIX)
+public class GatewayBootProperties {
+    /**
+     * Druid数据库连接池-属性前缀
+     */
+    public static final String PREFIX = "pivotal.cloud.gateway";
+
+    /**
+     * 是否校验总开关：true : 开启   false:关闭
+     */
+    private Boolean authEnabled;
+
+    /**
+     * 排除的鉴权接口路径
+     */
+    private List<String> excludePath;
+}
---
+++ pivotal-cloud-framework/pivotal-cloud-datacache/pivotal-cloud-datacache-j2cache/src/main/java/com/pivotal/cloud/datacache/j2cache/config/J2cacheDatacacheAspectAutoConfig.java
@@ -0,0 +1,14 @@
+package com.pivotal.cloud.datacache.j2cache.config;
+
+/**
+ * @className: com.pivotal.cloud.datacache.j2cache.config.J2cacheDatacacheAspectAutoConfig
+ * @projectName: 封装PivotalCloud项目-J2cacheDatacacheAspectAutoConfig类
+ * @module: PivotalCloud项目-J2cacheDatacacheAspectAutoConfig类，主要位于J2cacheDatacacheAspectAutoConfig模块的业务场景
+ * @content: J2cacheDatacacheAspectAutoConfig类，主要用于完成J2cacheDatacacheAspectAutoConfig的封装和定义.
+ * @author: Powered by Marklin
+ * @datetime: 2023-08-03 03:13
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 PivotalCloud Systems Incorporated. All rights reserved.
+ */
+public class J2cacheDatacacheAspectAutoConfig {
+}
